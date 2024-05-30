2b87ffbe82cae2c49c76e0f894d4a8d47828f870
Yana <150236797+yanasirazievaa@users.noreply.github.com>
2024-05-30 16:54:01 +0300

    проверка2

--- src/test/java/com/example/turkishsite/TurkishSiteApplicationTests.java
+++ src/test/java/com/example/turkishsite/TurkishSiteApplicationTests.java
@@ -16,14 +16,14 @@ class TurkishSiteApplicationTests {
 
     @DisplayName("Проверка на наличие рыбы в океанариуме")
     @ParameterizedTest(name = "{0} есть в океанариуме")
-    @ValueSource(strings = {"Кит", "Кас", "Немо"})
+    @ValueSource(strings = {"Кит", "Касатка", "Немо"})
     void checkIfFishExist(String fishName) {
         Assertions.assertTrue(oceanarium.exists(fishName));
     }
 
     @DisplayName("Проверка на отсутствие рыбы в океанариуме")
     @ParameterizedTest(name = "{0} нет в океанариуме")
-    @ValueSource(strings = {"Немо", "Щука", "Язь"})
+    @ValueSource(strings = {"Мегалодон", "Щука", "Язь"})
     void checkIfFishNotExist(String fishName) {
         Assertions.assertTrue(!oceanarium.exists(fishName));
     }
